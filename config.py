6b1931d9d7d285672a565f2747dbab256e3193f0
João Pedro <joaogood@outlook.com>
2024-01-23 10:22:26 -0300

    CLIMA V1.1

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ SERVER = 'localhost'
 DB = 'clima'
 
 #SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
-SQLALCHEMY_DATABASE_URI = 'postgres://sdbtqghohasdbs:593a3731f5e933150197aab8a808866f5f193d42eb95493cdd8bd0eb85798fd0@ec2-44-215-1-253.compute-1.amazonaws.com:5432/d94l6u8al1ivgo'
+SQLALCHEMY_DATABASE_URI = 'postgresql://sdbtqghohasdbs:593a3731f5e933150197aab8a808866f5f193d42eb95493cdd8bd0eb85798fd0@ec2-44-215-1-253.compute-1.amazonaws.com:5432/d94l6u8al1ivgo'
 
